072077c2152ec756f099cfc1f51dda04ea4cf154
vasanthkumar0706 <sundrapandian57@gmail.com>
2026-08-29 15:30:08 +0530

    Add files via upload

---
+++ if.py.git.py
@@ -0,0 +1,6 @@
+attendance=int(input("Enter attendance percentage:"))
+mark=int(input("enter mark percentage:"))
+if (attendance >=90 and mark >=75):
+    print("your Eligible")
+else:
+    print("your Not Eligible")
